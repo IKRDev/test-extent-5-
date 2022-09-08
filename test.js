8ca39ab83c29552d6af75cedbf56bdc0c474624f
IKRDev <112340060+IKRDev@users.noreply.github.com>
2022-09-08 11:21:00 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,53 @@
+class MinMaxtst {
+    constructor() {
+    }
+    
+    getInfo() {
+        return {
+            "id": "test",
+            "name": "MinMaxtest",
+            "blocks": [
+                        {
+                            "opcode": "min",
+                            "blockType": "reporter",
+                            "text": "[num1] min [num2]",
+                            "arguments": {
+                                "num1": {
+                                    "type": "number",
+                                    "defaultValue": "0"
+                                },
+                                "num2": {
+                                    "type": "number",
+                                    "defaultValue": "0"
+                                },
+                            }
+                        },
+                        {
+                            "opcode": "max",
+                            "blockType": "reporter",
+                            "text": "[num1] max [num2]",
+                            "arguments": {
+                                "num1": {
+                                    "type": "number",
+                                    "defaultValue": "0"
+                                },
+                                "num2": {
+                                    "type": "number",
+                                    "defaultValue": "0"
+                                },
+                            }
+                        },
+                ],
+        };
+    }
+    
+    min({num1,num2}) {
+        return Math.min(num1, num2)
+    }
+    
+    max({num1,num2}) {
+        return Math.max(num1, num2)
+    }
+}
+
+Scratch.extensions.register(new MinMaxtst())
